9a4500c17b12f6fedc1490116bc1d680788a5c5c
Patrick Ly <patrickly72@gmail.com>
2021-04-20 02:11:59 -0700

    uploaded generated html

--- index.js
+++ index.js
@@ -108,13 +108,12 @@ function getManager(){
        var managerID = response.managerid;
        var managerEmail = response.manageremail;
        var managerOfficeNumber = response.managerofficenumber;
-       var managerCard = `<div class="col-sm-4"><div class="card" style="width: 18rem;">
-       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eyeglasses" viewBox="0 0 16 16">
-  <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
-</svg>
+       var managerCard = `<div class="col-sm-4"><div class="card border-secondary mb-3" style="width: 18rem;">
        <div class="card-body">
-         <h5 class="card-title">${managerName}</h5>
-         <p class="card-text">Manager</p>
+         <h3 class="card-title">${managerName}</h3>
+         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-eyeglasses" viewBox="0 0 16 16">
+  <path d="M4 6a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+</svg><p class="card-text">Manager</p>
        </div>
        <ul class="list-group list-group-flush">
        <li class="list-group-item">ID: ${managerID}</li>  
@@ -161,13 +160,12 @@ function getEngineer(){
        var engineerID = response.engineerid;
        var engineerEmail = response.engineeremail;
        var github = response.github;
-       var engineerCard = `<div class="col-sm-4"><div class="card" style="width: 18rem;">
-       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cup" viewBox="0 0 16 16">
-  <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8zM13 2H2v10.5A1.5 1.5 0 0 0 3.5 14h8a1.5 1.5 0 0 0 1.5-1.5V2z"/>
-</svg>
+       var engineerCard = `<div class="col-sm-4"><div class="card border-secondary mb-3" style="width: 18rem;">
        <div class="card-body">
-         <h5 class="card-title">${engineerName}</h5>
-         <p class="card-text">Engineer</p>
+         <h3 class="card-title">${engineerName}</h3>
+         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cup" viewBox="0 0 16 16">
+  <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8zM13 2H2v10.5A1.5 1.5 0 0 0 3.5 14h8a1.5 1.5 0 0 0 1.5-1.5V2z"/>
+</svg><p class="card-text">Engineer</p>
        </div>
        <ul class="list-group list-group-flush">
        <li class="list-group-item">ID: ${engineerID}</li> 
@@ -214,13 +212,12 @@ function getIntern(){
        var internID = response.internid;
        var internEmail = response.internemail;
        var school = response.school;
-       var internCard = `<div class="col-sm-4"><div class="card" style="width: 18rem;">
-       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-dizzy-fill" viewBox="0 0 16 16">
-  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM4.146 5.146a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 1 1 .708.708l-.647.646.647.646a.5.5 0 1 1-.708.708L5.5 7.207l-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zm5 0a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zM8 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
-</svg>
+       var internCard = `<div class="col-sm-4"><div class="card border-secondary mb-3" style="width: 18rem;">
        <div class="card-body">
-         <h5 class="card-title">${internName}</h5>
-         <p class="card-text">Intern</p>
+         <h3 class="card-title">${internName}</h3>
+         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-emoji-dizzy-fill" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM4.146 5.146a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 1 1 .708.708l-.647.646.647.646a.5.5 0 1 1-.708.708L5.5 7.207l-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zm5 0a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 1 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 0-.708zM8 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"/>
+</svg><p class="card-text">Intern</p>
        </div>
        <ul class="list-group list-group-flush">
        <li class="list-group-item">ID: ${internID}</li> 
