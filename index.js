d2d6901c1645581c00ad0ab0158c17304145afb0
Patrick Ly <patrickly72@gmail.com>
2021-04-19 22:51:43 -0700

    added functions for prompt to start program

--- index.js
+++ index.js
@@ -5,6 +5,55 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 const teamMember =[];
-function addTeamMember {
+function addTeamMember () {
+    inquirer.prompt([
+       
+         {
+            type: 'list',
+            message: 'Would you like to add an Employee?',
+            name: 'addEmployee',
+            choices: ["YES", "NO"],
+          },])
+      .then ((response) => {
+        var addEmployee = response.addEmployee;
+        var html = ``;
+        if (addEmployee==="YES"){addEmployee()
+        };
+        if (addEmployee==="NO"){console.log(goodbye)};
+        
+        fs.writeFile('index.html', html , (err) => 
+        err ? console.error(err) : console.log('success')
+        
+        );
+    
+    
+    })
     
 }
+
+function addEmployee (){
+    inquirer.prompt([
+       
+        {
+           type: 'list',
+           message: 'What is the employee role?',
+           name: 'role',
+           choices: ["Manager", "Engineer", "Intern"],
+         },])
+     .then ((response) => {
+       var role = response.role;
+       var html = ``;
+       if (role==="Manager"){getManager};
+       if (role==="Engineer"){getEngineer};
+       if (role==="Intern"){getIntern};
+       
+       fs.writeFile('index.html', html , (err) => 
+       err ? console.error(err) : console.log('success')
+       
+       );
+   
+   
+   })
+
+}
+
